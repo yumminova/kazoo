76bd1ea049b753970343bf9eff079424d168edd9
lazedo <luis.azedo@factorlusitano.com>
2017-11-01 08:03:10 +0000

    freeswitch adds content-length

--- applications/ecallmgr/src/ecallmgr_fs_notify.erl
+++ applications/ecallmgr/src/ecallmgr_fs_notify.erl
@@ -186,7 +186,6 @@ send_mwi_update(JObj, Username, Realm, Node, Registration) ->
                       ,{<<"from-uri">>, From}
                       ,{<<"event-string">>, <<"message-summary">>}
                       ,{<<"content-type">>, <<"application/simple-message-summary">>}
-                      ,{<<"content-length">>, kz_term:to_binary(length(Body))}
                       ,{<<"body">>, kz_term:to_binary(Body)}
                       ],
             Resp = freeswitch:sendevent(Node, 'NOTIFY', Headers),
@@ -218,7 +217,6 @@ register_overwrite(JObj, Props) ->
                          ,{<<"from-uri">>, SipUri}
                          ,{<<"event-str">>, <<"registration-overwrite">>}
                          ,{<<"content-type">>, <<"text/plain">>}
-                         ,{<<"content-length">>, kz_term:to_binary(length(PrevBody))}
                          ,{<<"body">>, PrevBody}
                          ],
     NewContactHeaders = [{<<"profile">>, <<?DEFAULT_FS_PROFILE>>}
@@ -228,7 +226,6 @@ register_overwrite(JObj, Props) ->
                         ,{<<"from-uri">>, SipUri}
                         ,{<<"event-str">>, <<"registration-overwrite">>}
                         ,{<<"content-type">>, <<"text/plain">>}
-                        ,{<<"content-length">>, kz_term:to_binary(length(NewBody))}
                         ,{<<"body">>, NewBody}
                         ],
     case PrevContact of
